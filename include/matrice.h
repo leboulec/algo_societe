70af9c38c399e353e2fabf78c3e1558d792c2446
Clément Leboulenger <clement.leboulenger@etu.sorbonne-universite.fr>
2021-04-27 15:53:53 +0200

    Implémentation matrice

---
+++ include/matrice.h
@@ -0,0 +1,44 @@
+#ifndef __MATRICE_H__
+#define __MATRICE_H__
+
+/* Type matrice
+ * taille_m : nombre de colonne
+ * taille_n : nombre de ligne
+ * contenu[i][j] : i ligne j colonne
+ */
+typedef struct {
+	size_t taille_m;
+	size_t taille_n;
+	int **contenu;
+} matrice_t;
+
+// Matrice d'incidence
+typedef matrice_t matrice_inc_t;
+// Matrice d'adjacence
+typedef matrice_t matrice_adj_t;
+
+/* Build a matrice
+ * int m	 <-- nombre de colonnes
+ * int n	 <-- nombre de lignes
+ * matrice_t mat --> matrice construite
+ */
+matrice_t *build_matrice(size_t m, size_t n);
+
+/*
+ * Delete the matrice mat
+ */
+void delete_matrice(matrice_t *mat);
+
+/*
+ * Print the matrice mat
+ */
+void print_matrice(matrice_t *mat);
+
+/* Build a matrice d'incidence
+ * int *list_inc[2]	<-- liste d'incidence
+ * size_t n_list	<-- taille de la liste d'incidence
+ * n_vertex		<-- nombre de sommets
+ */
+matrice_inc_t *build_matrice_inc(int *liste_inc[2], size_t n_list, size_t n_vertex);
+
+#endif
---
+++ src/matrice.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "matrice.h"
+
+matrice_t *build_matrice(size_t m, size_t n)
+{
+	matrice_t *mat;
+	int **contenu;
+	size_t i;
+	int *colonne;
+
+	mat = (matrice_t *)malloc(sizeof(matrice_t)); 
+	if (mat == NULL)
+		return NULL;
+	contenu = (int **)malloc(n * sizeof(int *));
+	if (contenu == NULL) {
+		free(mat);
+		return NULL;
+	}
+
+	for (i = 0; i < n; i++) {
+		colonne = (int *)calloc(m ,sizeof(int));
+		if (colonne == NULL) {
+			if (i != 0)
+				for (--i; i >= 0; i--) {
+					free(contenu[i]);
+				}
+			free(contenu);
+			free(mat);
+			return NULL;
+		}
+		contenu[i] = colonne;
+	}
+	mat->taille_m = m;
+	mat->taille_n = n;
+	mat->contenu = contenu;
+	return mat;
+}
+
+void delete_matrice(matrice_t *mat)
+{
+	int i;
+	for (i = 0; i < mat->taille_n; i++)
+		free(mat->contenu[i]);
+	free(mat->contenu);
+	free(mat);
+}
+
+void print_matrice(matrice_t *mat)
+{
+	int i, j;
+	for (i = 0; i < mat->taille_n; i++) {
+		for (j = 0; j < mat->taille_m; j++) {
+			printf("%d ", mat->contenu[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+matrice_inc_t *build_matrice_inc(int *liste_inc[2], size_t n_list, size_t n_vertex)
+{
+	int i;
+	matrice_inc_t *mat = (matrice_inc_t *)build_matrice(n_list, n_vertex);
+	if (mat == NULL)
+		return NULL;
+	for (i = 0; i < n_list; i++) {
+		mat->contenu[i][liste_inc[i][0]];
+	}
+}
